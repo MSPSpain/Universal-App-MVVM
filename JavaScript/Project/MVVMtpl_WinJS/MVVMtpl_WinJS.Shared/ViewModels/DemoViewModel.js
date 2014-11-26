6c03b5314bc97243b4c54a47a7e0d34afda4de17
CKGrafico <quique@ckgrafico.com>
2014-11-26 18:26:39 +0100

    Add internet connection IU to the example

--- JavaScript/Project/MVVMtpl_WinJS/MVVMtpl_WinJS.Shared/ViewModels/DemoViewModel.js
+++ JavaScript/Project/MVVMtpl_WinJS/MVVMtpl_WinJS.Shared/ViewModels/DemoViewModel.js
@@ -3,18 +3,28 @@
 
     var DemoViewModel = WinJS.Class.derive(WinJS.ObservableObject,
         function () {
-            this.dependencies = "navigatorService";
+            this.dependencies = "navigatorService, networkService";
             this.observe();
         },
 
         {
             obs: {
-                hello: 'Hello world!'
+                hello: 'Hello world!',
+                connection: false
             },
 
             ready: function() {
                 // Dependencies loaded
+                this.checkInternet();
+            },
+
+            checkInternet: function () {
+                var self = this;
+                setInterval(function () {
+                    self.obs.connection = self.networkService.isOnline();
+                }, 1000);
             }
+                
         }
     );
 
--- JavaScript/Project/MVVMtpl_WinJS/MVVMtpl_WinJS.Windows/Views/DemoPage.js
+++ JavaScript/Project/MVVMtpl_WinJS/MVVMtpl_WinJS.Windows/Views/DemoPage.js
@@ -1,6 +1,8 @@
 ﻿(function (g, WinJS) {
     "use strict";
 
+    var appbar;
+
     WinJS.UI.Pages.define(Views.DemoPage, {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
@@ -9,6 +11,24 @@
             var viewModel = ViewModels.demoViewModel;
 
             WinJS.Binding.processAll(element, viewModel);
+
+            // Change appbar depending of internet
+            appbar = document.querySelector('.appbar').winControl;
+            viewModel.obs.bind('connection', this.onChangeConnection);
+            this.onChangeConnection(viewModel.obs.connection);
+            appbar.show();
+
+        },
+
+        onChangeConnection: function (connection) {
+            if (connection) {
+                appbar.hideCommands(['offlineCommand']);
+                appbar.showCommands(['onlineCommand']);
+            } else {
+                appbar.hideCommands(['onlineCommand']);
+                appbar.showCommands(['offlineCommand']);
+
+            }
         }
     });
 })(this, WinJS);
